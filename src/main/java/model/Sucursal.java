79712a07f89294eab4cf3398d16debfcbd8f3cdb
Ahrigo <87922044+Ahrigo@users.noreply.github.com>
2021-12-15 23:09:11 -0600

    sucursal model

--- src/main/java/model/Sucursal.java
+++ src/main/java/model/Sucursal.java
@@ -1,14 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model;
 
-/**
- *
- * @author arivera
- */
+
 public class Sucursal {
+    
+    private String id;
+    private String nombreSucursal;
+    private String Telefono;
+    private String Correo;
+    private String Direccion;
+
+    public Sucursal(String id, String nombreSucursal, String Telefono, String Correo, String Direccion) {
+        this.id = id;
+        this.nombreSucursal = nombreSucursal;
+        this.Telefono = Telefono;
+        this.Correo = Correo;
+        this.Direccion = Direccion;
+    }
+
+    public Sucursal() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombreSucursal() {
+        return nombreSucursal;
+    }
+
+    public void setNombreSucursal(String nombreSucursal) {
+        this.nombreSucursal = nombreSucursal;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+    
+    
     
 }
