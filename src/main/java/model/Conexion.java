8d51596d322d81510fc4bf61511c0a3317c7b48c
carloslalinde <54185636+carloslalinde@users.noreply.github.com>
2021-12-16 16:08:44 -0600

    Update Conexion.java

--- src/main/java/model/Conexion.java
+++ src/main/java/model/Conexion.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model;
 
 import java.lang.reflect.InvocationTargetException;
@@ -12,10 +8,7 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Luis
- */
+
 public class Conexion {
     
     
